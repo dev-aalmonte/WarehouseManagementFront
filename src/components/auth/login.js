ef37475f9a8882dfb04e962327ab49db1f9694a6
PC\Adonis <adonis0444@gmail.com>
2019-06-03 00:22:50 -0400

    Creaating Login component

--- src/components/auth/login.js
+++ src/components/auth/login.js
@@ -1,26 +1,47 @@
 import React, { Component } from 'react';
-import { reduxForm } from 'redux-form';
+import { reduxForm, Field } from 'redux-form';
+
+import { FormInput, FormButton } from '../formFields';
 
 import { Heading } from '../common/headings';
 import { PlaceholderImage } from '../common/image';
 
-class Login extends Component {
+class LoginForm extends Component {
     render() {
-        return(
+        const { className, handleSubmit } = this.props;
+        return (
+            <form onSubmit={handleSubmit} className={`${className} login-form`}>
+                <Field className='login-form__email' type='email' name='email' title='Email' placeholder='Email' component={FormInput}/>
+                <Field className='login-form__password' type='password' name='password' title='Password' placeholder='Password' component={FormInput}/>
+                <Field className='login-form__login' type='submit' name='login' title='Login' onClick={() => console.log('submiting')} component={FormButton}/>
+
+            </form>
+        )
+    }
+}
+
+class Login extends Component {
+    onSubmit = (fields) => {
+        console.log(fields);
+    }
+
+    render() {
+        return (
             <div className='login'>
                 <div className='login__background'>
                     <PlaceholderImage className='login__background__image' width='1920' height='1080' />
                 </div>
-                <div className='login__form'>
-                    <Heading className='login__form__heading' text='Login'/>
+                <div className='login__content'>
+                    <Heading className='login__content__heading' text='Login'/>
+                    <LoginForm onSubmit={this.onSubmit} className='login__content__form' />
                 </div>
             </div>
         )
     }
 }
 
-Login = reduxForm({
+LoginForm = reduxForm({
     form: 'Login'
-})(Login);
+})(LoginForm);
 
 export default Login;
