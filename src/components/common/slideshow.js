8c72039dce2abdda908432743050665f568bbe6d
PC\Adonis <adonis0444@gmail.com>
2019-06-06 08:09:19 -0400

    Adding the functionality to the slideshow controls

--- src/components/common/slideshow.js
+++ src/components/common/slideshow.js
@@ -2,26 +2,79 @@ import React, { Component } from 'react';
 import Icon from './icon';
 
 class Slideshow extends Component {
+    constructor (props) {
+        super(props);
+
+        this.state = {
+            imageList: props.imgs,
+            currentImageIndex: 0,
+            lastIndex: props.imgs.length - 1,
+        }
+    }
+
+    selectImage = (event, index) => {
+        document.querySelectorAll('.slideshow__bulletpoint__bullet').forEach(element => {
+            element.classList.remove('active');
+        });
+        event.target.classList.add('active');
+        this.setState({currentImageIndex: index});
+    }
+    
+    moveFoward = () => {
+        document.querySelectorAll('.slideshow__bulletpoint__bullet').forEach(element => {
+            element.classList.remove('active');
+        });
+        const { currentImageIndex, lastIndex } = this.state;
+        let index;
+        if(currentImageIndex == lastIndex) {
+            index = 0;
+        }
+        else {
+            index = currentImageIndex + 1;
+        }
+        document.querySelectorAll('.slideshow__bulletpoint__bullet')[index].classList.add('active');
+        this.setState({currentImageIndex: index});
+
+    }
+    
+    moveBackward = () => {
+        document.querySelectorAll('.slideshow__bulletpoint__bullet').forEach(element => {
+            element.classList.remove('active');
+        });
+        const { currentImageIndex, lastIndex } = this.state;
+        let index;
+        if(currentImageIndex == 0) {
+            index = lastIndex;
+        }
+        else {
+            index = currentImageIndex - 1;
+        }
+        document.querySelectorAll('.slideshow__bulletpoint__bullet')[index].classList.add('active');
+        this.setState({currentImageIndex: index});
+    }
+    
     render() {
-        const { className, imgs } = this.props;
+        const { className } = this.props;
+        const { imageList, currentImageIndex } = this.state;
         return (
             <div className={`${className} slideshow`}>
                 <div className='slideshow__image-container'>
-                    <img className='slideshow__image-container__image' src={null} />
+                    <img className='slideshow__image-container__image' src={imageList[currentImageIndex]} />
                 </div>
-                <div className='slideshow__back-control'>
+                <div onClick={this.moveBackward} className='slideshow__back-control'>
                     <Icon className='slideshow__back-control__icon' icon='angle-left' />
                 </div>
                 <div className='slideshow__bulletpoint'>
                     {
-                        imgs.map((img, index) => {
+                        imageList.map((img, index) => {
+                            const active = index == 0 ? 'active' : '';
                             return (
-                                <div key={index} className='slideshow__bulletpoint__bullet'></div>
+                                <div key={index} className={`slideshow__bulletpoint__bullet ${active}`} onClick={(event) => this.selectImage(event, index)}></div>
                             )
                         })
                     }
                 </div>
-                <div className='slideshow__foward-control'>
+                <div onClick={this.moveFoward} className='slideshow__foward-control'>
                     <Icon className='slideshow__foward-control__icon' icon='angle-right' />
                 </div>
             </div>
