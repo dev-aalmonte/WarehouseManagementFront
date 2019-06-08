1c3ece87fbb80a084f4a147ee7bd30e118d22676
PC\Adonis <adonis0444@gmail.com>
2019-06-08 12:14:33 -0400

    Minor changes in the style of Client Component

--- src/components/app.js
+++ src/components/app.js
@@ -3,13 +3,13 @@ import React, { Component } from 'react';
 import Header from './common/header';
 
 import { library } from '@fortawesome/fontawesome-svg-core';
-import { faQuestionCircle, faUsers, faArchive, faBoxes, faWarehouse, faSearch, faAngleLeft, faAngleRight, faAngleDown } from '@fortawesome/free-solid-svg-icons';
+import { faQuestionCircle, faUsers, faArchive, faBoxes, faWarehouse, faSearch, faAngleLeft, faAngleRight, faAngleDown, faPlus } from '@fortawesome/free-solid-svg-icons';
 
 import Modal from './common/modal';
 import ProductDetail from './products/productDetail';
 import ProductAdd from './products/productAdd';
 
-library.add(faQuestionCircle, faUsers, faArchive, faBoxes, faWarehouse, faSearch, faAngleLeft, faAngleRight, faAngleDown);
+library.add(faQuestionCircle, faUsers, faArchive, faBoxes, faWarehouse, faSearch, faAngleLeft, faAngleRight, faAngleDown, faPlus);
 
 export default class App extends Component {
     render() {
--- src/components/clients/client.js
+++ src/components/clients/client.js
@@ -3,6 +3,7 @@ import React, { Component } from 'react';
 import { Heading, Text } from '../common/headings';
 import Table from '../common/table';
 import Searchbar from '../common/searchbar';
+import { FormSmallButton } from '../formFields';
 
 class Client extends Component {
     render() {
@@ -12,7 +13,10 @@ class Client extends Component {
                 <Heading className='client__heading'>Client {match.params.id}</Heading>
                 <Text className='client__description' >Lorem ipsum dolor sit amet, consectetur adipiscing elit. In tellus ex, finibus id vehicula eu, tempus non mauris. Sed nec purus vitae augue volutpat viverra non quis ligula. Ut eget mauris id tellus sollicitudin sagittis. Fusce euismod dui vel nulla aliquam, id luctus mauris sodales. Phasellus pharetra cursus lacus in pulvinar. Quisque posuere diam non massa sodales aliquam. Suspendisse pellentesque fermentum nibh ut euismod. Cras ac pellentesque turpis. Morbi at orci ultrices, congue lorem in, sodales eros.</Text>
                 <Heading className='client__order-heading'>Orders</Heading>
-                <Searchbar className='client__order-searchbar' placeholder='Search for Order' />
+                <div className='client__order-searchbar-container'>
+                    <Searchbar className='client__order-searchbar-container__searchbar' placeholder='Search for Order' />
+                    <FormSmallButton className='client__order-searchbar-container__button' type='button' icon='plus'/>
+                </div>
                 <Table className='client__order-table'/>
             </div>
         )
