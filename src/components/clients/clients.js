1bafbd26fc49986076bb800417e63a623da8787b
PC\Adonis <adonis0444@gmail.com>
2019-07-28 23:03:41 -0400

    Add CRUD buttons and heading to clients component]

--- src/components/clients/clients.js
+++ src/components/clients/clients.js
@@ -1,13 +1,22 @@
 import React, { Component } from 'react';
 
+import { FormSmallButton } from '../formFields';
+
 import Searchbar from '../common/searchbar';
 import Table from '../common/table';
+import { Heading } from '../common/headings';
 
 class Clients extends Component {
     render() {
         return(
             <div className='clients'>
+                <Heading className='clients__heading'>Clients</Heading>
                 <Searchbar className='clients-searchbar' placeholder='Search a Client' />
+                <div className='clients__buttoms'>
+                    <FormSmallButton onClick={() => 0} className='clients__buttoms__button' type='button' icon='minus'/>
+                    <FormSmallButton onClick={() => 0} className='clients__buttoms__button' type='button' icon='edit'/>
+                    <FormSmallButton onClick={() => 0} className='clients__buttoms__button' type='button' icon='plus'/>
+                </div>
                 <Table className='clients__table' />
             </div>
         )
