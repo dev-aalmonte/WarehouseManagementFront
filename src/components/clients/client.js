eb2b6a5014878a5e4b89b391ae9ea25191575a06
PC\Adonis <adonis0444@gmail.com>
2019-07-30 23:15:57 -0400

    Display main information of the client In the Client Component

--- src/components/clients/client.js
+++ src/components/clients/client.js
@@ -1,16 +1,23 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import * as actions from '../../actions';
 
-import { Heading, Text } from '../common/headings';
+import { Heading, Text, SmallHeading } from '../common/headings';
 import Table from '../common/table';
 import Searchbar from '../common/searchbar';
 import { FormSmallButton } from '../formFields';
 
 class Client extends Component {
+
+    componentWillMount() {
+        this.props.selectSingleClientFromDB(this.props.match.params.id);
+    }
+
     render() {
-        const { match } = this.props;
         return(
             <div className='client'>
-                <Heading className='client__heading'>Client {match.params.id}</Heading>
+                <Heading className='client__heading'>{this.props.selected_client.first_name} {this.props.selected_client.last_name}</Heading>
+                <SmallHeading className='client__small-heading' size='xsmall'>{this.props.selected_client.email}</SmallHeading>
                 <Text className='client__description' >Lorem ipsum dolor sit amet, consectetur adipiscing elit. In tellus ex, finibus id vehicula eu, tempus non mauris. Sed nec purus vitae augue volutpat viverra non quis ligula. Ut eget mauris id tellus sollicitudin sagittis. Fusce euismod dui vel nulla aliquam, id luctus mauris sodales. Phasellus pharetra cursus lacus in pulvinar. Quisque posuere diam non massa sodales aliquam. Suspendisse pellentesque fermentum nibh ut euismod. Cras ac pellentesque turpis. Morbi at orci ultrices, congue lorem in, sodales eros.</Text>
                 <Heading className='client__order-heading'>Orders</Heading>
                 <div className='client__order-searchbar-container'>
@@ -23,4 +30,9 @@ class Client extends Component {
     }
 }
 
-export default Client;
+function mapStateToProps(state) {
+    const { selected_client, pagination } = state.client;
+    return { selected_client, pagination };
+}
+
+export default connect(mapStateToProps, actions)(Client);
